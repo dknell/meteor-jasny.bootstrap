783292e6babc3a3d6c675ea5c35d385e74e4d7dc
David Knell <dk45@mac.com>
2013-02-28 16:58:45 -0500

    updated description

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 Package.describe({
-	summary: "Latest version of Bootstrap by Jasny (2.3.0-j4)"
+	summary: "Latest version of Bootstrap by Jasny (2.3.0-j4). Also includes the iconic fonts."
 });
 
 Package.on_use(function (api){
